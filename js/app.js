b8a3d7107f14ae762d3b463342f90e8cb4bf3e34
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-05 16:40:34 +0100

    adds noConflict jQuery wrapper function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-$( ".nav-toggle" ).click(function() {
-  $(this).toggleClass("open");
-  $("nav").fadeToggle(100);
+jQuery(document).ready(function($) {
+    // $() will work as an alias for jQuery() inside of this function
 
-  return false;
+    $(document).foundation();
+    $( ".nav-toggle" ).click(function() {
+      $(this).toggleClass("open");
+      $("nav").fadeToggle(100);
+
+      return false;
+    });
 });
