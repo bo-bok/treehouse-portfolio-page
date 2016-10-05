f7ebe00a46fce686613dd1b8a194fa9808f1a92f
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-05 16:35:35 +0100

    imports js script by linking function in functions.php

--- functions.php
+++ functions.php
@@ -9,5 +9,12 @@
     wp_enqueue_style('google_fonts', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic');
 
   }
-add_action( 'wp_enqueue_scripts','wpt_theme_style' );
+  add_action( 'wp_enqueue_scripts','wpt_theme_style' );
+
+  function wpt_theme_js() {
+    wp_enqueue_script('modernizr_js', get_template_directory_uri() . 'js/modernizr.js', '', '', false);
+    wp_enqueue_script('foundation_js', get_template_directory_uri() . 'js/foundation.min.js', array('jquery'), '', true);
+    wp_enqueue_script('main_js', get_template_directory_uri() . 'js/app.js', array('jquery', 'foundation_js'), '', true);
+  }
+  add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
 ?>
