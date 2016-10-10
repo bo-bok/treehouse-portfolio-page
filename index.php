71ad4cc4e62cad761980236c5fa3c2356e1df172
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-10 10:32:12 +0100

    add page-sidebar code & TH recap

--- index.php
+++ index.php
@@ -4,11 +4,16 @@
 <section class="row">
   <div class="small-12 columns text-center">
     <div class="leader">
+
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
         <h1><?php the_title(); ?></h1>
         <p><?php the_content(); ?></p>
+
       <?php endwhile; else : ?>
-      	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+        <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
       <?php endif; ?>
 
     </div>
@@ -18,3 +23,4 @@
 
 
 <?php get_footer(); ?>
+ 
