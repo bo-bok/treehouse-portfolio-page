c18a0ca16fef52c94275768526b87ba3ae9ba76d
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-10 11:29:27 +0100

    wp nav/menu setup so that admin panel recognises our menu

--- functions.php
+++ functions.php
@@ -1,22 +1,35 @@
 <?php
 
-  function wpt_theme_styles() {
+add_theme_support( 'menus' );
 
-    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
-    // wp_enqueue_style('normalize_css', get_template_directory_uri() . '/css/normalize.css');
-    wp_enqueue_style( 'google_fonts', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
-    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+function register_theme_menus() {
 
-  }
-  add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+  register_nav_menus(
+    array(
+      'primary-menu' => __( 'Primary Menu' )
+    )
+  );
 
-  function wpt_theme_js() {
+}
+add_action( 'init', 'register_theme_menus' );
 
-    wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false) ;
-    wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true) ;
-    wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true) ;
+function wpt_theme_styles() {
 
-  }
-  add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
+  wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
+  // wp_enqueue_style('normalize_css', get_template_directory_uri() . '/css/normalize.css');
+  wp_enqueue_style( 'google_fonts', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
+  wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_styles' );
+
+function wpt_theme_js() {
+
+  wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false) ;
+  wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true) ;
+  wp_enqueue_script( 'main_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true) ;
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_theme_js' );
 
 ?>
