ea39cd01045734ff0be0205a8922c176f65e4a8c
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-12 18:15:17 +0100

    adds code to single post template, bug resides

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,42 +1,40 @@
-<?php
-/*
-  Template Name: Left Sidebar
-*/
-?>
-
 <?php get_header(); ?>
-
 
 <section class="two-column row no-max pad">
   <div class="small-12 columns">
     <div class="row">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
       <!-- Primary Column -->
       <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
         <div class="primary">
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <h1><?php the_title(); ?></h1>
-          <hr />
-          <p><?php the_content(); ?></p>
-        <?php endwhile; else : ?>
-        	<p><?php _e( 'Sorry, no pages found.' ); ?></p>
-        <?php endif; ?>
-
+          <?php the_field('images'); ?>
         </div>
       </div>
 
       <!-- Secondary Column -->
       <div class="small-12 medium-4 medium-pull-8 columns">
         <div class="secondary">
-          <h2 class="module-heading">Sidebar</h2>
+
+          <h1><?php the_title(); ?></h1>
+          <p><?php the_field('description'); ?></p>
+
+          <hr>
+          <p>
+            <?php previous_post_link(); ?> -
+            <a href="<?php blognfo('url');  ?>/portfolio">Back to Portfolio</a> -
+            <?php next_post_link(); ?>
+          </p>
+
         </div>
       </div>
+    <?php endwhile(); endif; ?>
+
     </div>
-  </div>
+  <!-- </div> -->
 </section>
 
 
 
 <?php get_footer(); ?>
-
-<!-- add blog code -->
