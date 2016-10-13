bcdf38f4b5da8ede8daaeb1095c5008779053fdf
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-13 19:07:53 +0100

    adds dynamic code

--- sidebar-page.php
+++ sidebar-page.php
@@ -1,6 +1,14 @@
 <!-- Secondary Column -->
 <div class="small-12 medium-4 medium-pull-8 columns">
   <div class="secondary">
-    <h2 class="module-heading">Page Sidebar 1</h2>
+
+    <?php if(  !dynamic_sidebar( 'page ' )  ): ?>
+      <!--  automatically output the sidebar if it exists & if not will output message telling them to set it up-->
+
+      <h2 class="module-heading">Sidebar Setup</h2>
+      <p>Please add widgets via the admin area!</p>
+
+    <?php endif;  ?>
+
   </div>
 </div>
