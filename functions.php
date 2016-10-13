23538c7a0221c10f0ba45e074f9935e13cffc663
Jessica Salmon <jessicasalmon05@gmail.com>
2016-10-13 17:33:06 +0100

    adds code for widget

--- functions.php
+++ functions.php
@@ -23,6 +23,26 @@ function register_theme_menus() {
 }
 add_action( 'init', 'register_theme_menus' );
 
+
+// this function creates a widget
+function wpt_create_widget( $name, $id, $description ) {
+
+ register_sidebar(array(
+   'name' => __( $name ),
+   'id' => $id,
+   'description' => __( $description ),
+   'before_widget' => '<div class="widget">',
+   'after_widget' => '</div>',
+   'before_title' => '<h2 class="module-heading">',
+   'after_title' => '</h2>'
+ ));
+
+}
+
+wpt_create_widget( 'Page Sidebar', 'page', 'Displays on the side of pages with a sidebar' );
+wpt_create_widget( 'Blog Sidebar', 'blog', 'Displays on the side of pages in the blog section' );
+
+
 // this function loads the style (css) files
 function wpt_theme_styles() {
 
